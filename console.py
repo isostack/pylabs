f97e740e9e0119f7f3b91cde0e807b312a83b34b
frederick ohene gh <66055564+isostack@users.noreply.github.com>
2022-07-30 17:53:21 +0000

    update

--- console.py
+++
@@ -1,9 +0,0 @@
-def gen_pass():
-    import random
-    # Generate a random password
-    password = ''
-    for i in range(0,8):
-        password += chr(random.randint(33,126))
-    print(password)
-
-gen_pass()
--- main.py
+++ main.py
@@ -1,63 +1,24 @@
-from tkinter import *
-from tkinter import messagebox
-import pyperclip
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
-def gen_pass():
-    import random
-    # Generate a random password
-    pass_inpt.delete(0,END)
-    password = ''.join([chr(random.randint(48,126)) for _ in range(0,8)])
-    pass_inpt.insert(0,password)
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-def add_data():
-    # Get the password
-    item_name = wnt_inpt.get()
-    item_mail = mail_inpt.get()
-    item_pass = pass_inpt.get()
-    # Check if the password is empty
-    if len(item_pass) == 0 or len(item_name) == 0 or len(item_mail) == 0:
-        messagebox.showinfo('Error','You have unfilled fields in the form!')
-    else:    
-        state = messagebox.askokcancel(title = item_name , message=f"Website: {item_name}'s \nPassword: {item_pass} \nEmail: {item_mail} \n\nIs this correct?")
-        # Save the password
-        
-        if state:
-            with open('passwords.txt', 'a') as file:
-                item = item_name + ' | ' + item_mail + ' | ' + item_pass + '\n'
-                file.write(item)
-        # Clear the password
-            pyperclip.copy(item_pass)
-            pass_inpt.delete(0,END)
-            wnt_inpt.delete(0,END)
-# ---------------------------- UI SETUP ------------------------------- #
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
 
-window = Tk()
-window.title("Password Manager")
-window.config(padx=50,pady=50)
-app_image = PhotoImage(file = 'logo.png')
-app_canv = Canvas(width=200 , height=200)
-app_canv.create_image(100,100 , image=app_image)
-app_canv.grid(row=0,column=1)
+from ast import excepthandler
+from xml.etree.ElementTree import TreeBuilder
+import pandas
 
-webname_txt = Label(window,text="Website Name:")
-webname_txt.grid(row=1,column=0)
-mail_txt = Label(window,text="Email:")
-mail_txt.grid(row=2,column=0)
-pass_txt = Label(window,text="Password:")
-pass_txt.grid(row=3,column=0)
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+#TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
-wnt_inpt = Entry(window,width=55)
-wnt_inpt.grid(row=1,column=1,columnspan=2)
-wnt_inpt.focus()
-mail_inpt = Entry(window,width=55)
-mail_inpt.grid(row=2,column=1,columnspan=2)
-mail_inpt.insert(END,"gamma@gmail.com")
-pass_inpt = Entry(window,width=36)
-pass_inpt.grid(row=3,column=1)
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def check_board():
+    word = input("Enter a word: ").upper()
+    try:  
+        output_list = [phonetic_dict[letter] for letter in word] 
+    except KeyError: 
+        print("Invalid input or text")
+        check_board()
+    else:  
+        print(output_list)
 
-gen_btn = Button(window,text="Generate Password",command=gen_pass)
-gen_btn.grid(row=3,column=2)
-
-save_btn = Button(window,width=46, text="Add Item", command=add_data)
-save_btn.grid(row=4,column=1 , columnspan=2)
-window.mainloop()
+check_board()
