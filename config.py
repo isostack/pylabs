c7307618d46f14339bc0bbff5406e687636b078a
owner <dezynarh@gmail.com>
2022-08-10 09:00:48 -0400

    NewCode

--- config.py
+++ config.py
@@ -1,1 +1,27 @@
-print(1 + 3)
+import requests
+import hourly
+
+# endpoint = "https://api.openweathermap.org/data/3.0/onecall"
+# api_key = '40e7987b75c52fe5f40c2e2ff17ac805'
+# parameters = {
+#     "lat": 5.551167,
+#     "lon": 0.200450,
+#     "appid": api_key,
+# }
+
+# response = requests.get(endpoint, params=parameters)
+# response.raise_for_status() 
+# print(response.json())
+
+h_data_main = hourly.hour_data[:10]
+
+will_rain = False
+
+condition_codes = [item["weather"][0]["id"] for item in h_data_main]
+for item in condition_codes:
+    if item < 700:
+        will_rain = True 
+
+if will_rain:
+    print("Bring an umbrella")
+
---
+++ twilio.py
@@ -0,0 +1,20 @@
+import os
+from twilio.rest import Client
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+# account_sid = os.environ['TWILIO_ACCOUNT_SID']
+# auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+account_sid = 'AC2dccdf48a1858bb3d441885dc0425840'
+auth_token = '2b2f8f36c2a3591607ecb75789ea942a'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+15017122661',
+                     to='+15558675310'
+                 )
+
+print(message.sid)
