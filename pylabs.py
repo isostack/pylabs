a35b85f72f0d8192a5088a52ddfd3fc1a363b06d
frederick ohene gh <66055564+isostack@users.noreply.github.com>
2022-10-31 07:23:34 +0000

    Project Update

--- pylabs.py
+++ pylabs.py
@@ -1,10 +1,11 @@
-print("hello world")
-print("Hellow world")
+import math
+def area_of_polygon_inside_circle(circle_radius, number_of_sides):
+    """Return the area of a regular polygon inside a circle of radius circle_radius
+    with number_of_sides sides.
+    """
+    side_length = 2 * circle_radius * math.sin(math.pi / number_of_sides)
+    apothem = circle_radius * math.cos(math.pi / number_of_sides)
+    area = number_of_sides * side_length * apothem / 2
+    return round(area, 3)
 
-#!/usr/bin/env python3
-# Path: pylabs.py
-print("hello world")
-result = 2 + 2
-print(result)
-
-hello = "hello world"
+print(area_of_polygon_inside_circle(3, 3))
